5992c6330d8ce44448160d55b89be6b3db95e6ce
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-01-23 13:41:19 +0100

    ok

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+//Exo1
+$name = "john";
+echo $name
+
+//Exo2
+$age = 25;
+$weight = 60;
+$height = 178;
+echo $age;
+echo $weight;
+echo $height;
+?>  
+</body>
+</html>
+
